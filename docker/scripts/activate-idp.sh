11dd21d4321df16e7430aa7f79b301fd60c922e8
Andy Brody <andrew.brody@gsa.gov>
2019-04-29 18:39:33 -0400

    Activate: support using a different git ref.

--- docker/scripts/activate-idp.sh
+++ docker/scripts/activate-idp.sh
@@ -6,10 +6,16 @@ run() {
     "$@"
 }
 
+if [ -n "${DEVOPS_ACTIVATE_GIT_REF-}" ]; then
+    cd /etc/login.gov/repos/identity-devops
+    run git fetch
+    run git checkout "$DEVOPS_ACTIVATE_GIT_REF"
+fi
+
 # Run chef to finalize configuration
 # TODO figure out how to remove recipe[passenger::daemon], by making idp_web
 # not depend on service[passenger]
 run id-chef-client main -o 'recipe[passenger::daemon],recipe[instance_certificate::generate],recipe[service_discovery::register],recipe[login_dot_gov::idp_web]'
 
 # Exec and start up nginx
-run exec /opt/nginx/bin/nginx -g 'daemon off;'
+run exec /opt/nginx/sbin/nginx -g 'daemon off;'
