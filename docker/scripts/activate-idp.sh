5b3195fb5c11e6435f09839970205b21f59e739f
Andy Brody <andrew.brody@gsa.gov>
2019-04-29 18:35:08 -0400

    Try adding passenger recipe.

--- docker/scripts/activate-idp.sh
+++ docker/scripts/activate-idp.sh
@@ -7,7 +7,9 @@ run() {
 }
 
 # Run chef to finalize configuration
-run id-chef-client main -o 'recipe[instance_certificate::generate],recipe[service_discovery::register],recipe[login_dot_gov::idp_web]'
+# TODO figure out how to remove recipe[passenger::daemon], by making idp_web
+# not depend on service[passenger]
+run id-chef-client main -o 'recipe[passenger::daemon],recipe[instance_certificate::generate],recipe[service_discovery::register],recipe[login_dot_gov::idp_web]'
 
 # Exec and start up nginx
 run exec /opt/nginx/bin/nginx -g 'daemon off;'
